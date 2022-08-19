6fc525fe848601bb79cc64e70620c0c707d5e7a4
ken8891837 <106743263+ken8891837@users.noreply.github.com>
2022-08-19 21:42:16 +0800

    Add files via upload

---
+++ 作业.py
@@ -0,0 +1,20 @@
+festival = input('请输入节日')
+
+#注释：在终端中询问用户，并输出提示语句：请输入节日
+
+if festival == '中秋节':
+    print('中秋节的习俗')
+else:
+    if festival == '元宵节':
+        print('元宵节的习俗')
+    else:
+            if festival == '春节':
+                print('春节的习俗')
+            else:
+                if festival == '七夕节':
+                    print('七夕节的习俗')
+                else:
+                    if festival == '端午节':
+                        print('端午节的习俗')
+                    else:
+                        print('你输入的节日存在错误，请检查后再试，注意：只支持中国传统节日')
